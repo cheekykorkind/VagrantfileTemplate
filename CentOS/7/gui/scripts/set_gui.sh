9db4e69661186040821f6dad68c0e745c24c5a21
cheekyorkind <cheekorkind@gmail.com>
2020-01-09 23:16:19 +0900

    change set_git.sh location

--- CentOS/7/gui/scripts/set_gui.sh
+++ CentOS/7/gui/scripts/set_gui.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -ex
-
 echo "set GUI"
 
-yum groupinstall -y 'gnome desktop'
-yum install -y 'xorg*'
-yum remove -y initial-setup initial-setup-gui
-systemctl isolate graphical.target
+# yum groupinstall -y 'gnome desktop'
+# yum install -y 'xorg*'
+# yum remove -y initial-setup initial-setup-gui
+# systemctl isolate graphical.target
+# systemctl set-default graphical.target
+
+yum -y groupinstall "GNOME Desktop"
+yum -y epel-release
 systemctl set-default graphical.target
+systemctl get-default
+shutdown -r now
