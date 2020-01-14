dbc1ea313cae92e50a32b1d11baad7e5a9153c44
cheekyorkind <cheekorkind@gmail.com>
2020-01-14 22:07:12 +0900

    should be added gui true

--- CentOS/7/gui/scripts/set_gui.sh
+++ CentOS/7/gui/scripts/set_gui.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit 0
+set -ex
 
-echo "set GUI"
+echo '##########################################################################'
+echo '##### About to run install-gnome-gui.sh script ##################'
+echo '##########################################################################'
 
-# yum groupinstall -y 'gnome desktop'
-# yum install -y 'xorg*'
-# yum remove -y initial-setup initial-setup-gui
-# systemctl isolate graphical.target
-# systemctl set-default graphical.target
+# https://codingbee.net/tutorials/vagrant/vagrant-enabling-a-centos-vms-gui-mode
+yum groupinstall -y 'gnome desktop'
+yum install -y 'xorg*'
 
-yum -y groupinstall "GNOME Desktop"
-yum -y epel-release
-systemctl set-default graphical.target
-systemctl get-default
-shutdown -r now
+# https://unix.stackexchange.com/questions/181009/gnome-license-not-accepted-issue-when-system-has-been-rebooted
+yum remove -y initial-setup initial-setup-gui
+
+systemctl isolate graphical.target
+systemctl set-default graphical.target   # to make this persistant
