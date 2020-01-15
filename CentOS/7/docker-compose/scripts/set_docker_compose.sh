e3898df69f14492ad4e2b1ace0416b2ce2aaeac5
cheekyorkind <cheekorkind@gmail.com>
2020-01-16 00:11:44 +0900

    pip will not support. should be changed

--- CentOS/7/docker-compose/scripts/set_docker_compose.sh
+++ CentOS/7/docker-compose/scripts/set_docker_compose.sh
@@ -14,9 +14,6 @@ systemctl enable docker.service
 systemctl start docker.service
 
 # Docker Compose install
-yum -y install epel-release
-yum -y install python-pip
-
-pip install --upgrade pip
-yes | sudo pip install docker-compose
-yum -y upgrade python*
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
