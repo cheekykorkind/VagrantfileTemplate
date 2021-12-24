dac6fac2eb478ffbed31174191ffc03edd20f78d
cheekykorkind <cheekorkind@gmail.com>
2021-12-24 08:21:14 -0500

    docker compose version up

--- Android/Ubuntu/18.04/scripts/set_docker_compose.sh
+++ Android/Ubuntu/18.04/scripts/set_docker_compose.sh
@@ -19,5 +19,5 @@ systemctl start docker.service
 
 
 # Docker Compose install
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
